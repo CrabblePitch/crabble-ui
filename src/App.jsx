a6af9093b3f735b75fde8b46e5c9a52d4f309c37
anilhelvaci <anilhelvaci96@gmail.com>
2023-07-26 09:34:34 +0300

    Initial work for Agoric UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import OfferSignerBridge from "./OfferSignerBridge.jsx";
 
+/**
+ * Purpose: This component must be able to;
+ * - Connect to Keplr
+ * - Using the info coming from Keplr, must establish a connection with the agoric wallet
+ *
+ * To connect to Keplr;
+ * - Can establish a chainStorageWatcher(makeAgoricChainStorageWatcher). Required arguments to achieve this;
+ *  - A rpc address
+ *  - Chain Name
+ *  - Unmarshaller: makeImportContext()
+ * - Use makeAgoricWalletConnection(chainStorageWatcher) for connecting Keplr
+ *
+ * It is also possible to connect to Keplr by just using getKeplrAddress from @agoric/web-components where we only
+ * need a chainId which we can get from the network-config
+ *
+ *  Once we have a Keplr connection, we can feed OfferSignerBridge with the required parameters as props so it can
+ *  connect to the Agoric wallet.
+ *
+ *  A working network-config:
+ *  {
+ *   "chainName": "agoriclocal",
+ *   "rpcAddrs": ["http://localhost:26657"]
+ *  }
+ *
+ *  Below props are necessary for OfferSignerBridge:
+ *  - address: Get this from Keplr connection
+ *  - chainId:
+ */
 function App() {
   const [count, setCount] = useState(0)
 
