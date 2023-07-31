d044be5a9a1f4e181fa760bc5471ccf490571304
anilhelvaci <anilhelvaci96@gmail.com>
2023-07-31 09:38:19 +0300

    Remove hardcoded lockdown

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import '../installSesLockdown'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@endo/eventual-send/shim.js'; // adds support needed by E
 import App from './App.jsx'
 import './index.css'
 
