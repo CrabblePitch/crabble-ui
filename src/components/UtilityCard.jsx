7d1262e34aac47747da6c25d81796b16ae147da1
Grigori Grigoryan <grigori.grigoryan@bytepitch.com>
2023-09-28 23:24:58 +0400

    modal close fix

--- src/components/UtilityCard.jsx
+++ src/components/UtilityCard.jsx
@@ -1,27 +1,23 @@
-import Typography from "@mui/material/Typography";
-import { Card, CardActionArea, CardContent, CardMedia, Stack } from "@mui/material";
-import PhaseChip from "./PhaseChip.jsx";
-import { displayAmount, getValueFromSet } from "../utils/helpers.js";
-import useStore from "../store/store.js";
-import { stringifyValue } from "@agoric/ui-components";
-import { AssetKind } from "@agoric/ertp";
-import { useEffect } from "react";
+import Typography from '@mui/material/Typography';
+import { Card, CardActionArea, CardContent, CardMedia, Stack } from '@mui/material';
+import PhaseChip from './PhaseChip.jsx';
+import { displayAmount, getValueFromSet } from '../utils/helpers.js';
+import useStore from '../store/store.js';
+import { stringifyValue } from '@agoric/ui-components';
+import { AssetKind } from '@agoric/ertp';
+import { useEffect } from 'react';
 
 const cardDefaults = {
     bgcolor: 'surface.main',
     border: 1,
-    color: 'onSurface.main', boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.75)'
-}
+    color: 'onSurface.main',
+    boxShadow: '0px 0px 20px 0px rgba(0,0,0,0.75)',
+};
 
-const UtilityCard = ({
-                         data,
-                         onCardClick,
-                         detailed = true,
-                         cardStyles = {}
-                     }) => {
-    const wallet = useStore(state => state.wallet);
-    const notifyUser = useStore(state => state.notifyUser);
-    const getKeywordFromBrand = useStore(state => state.getKeywordFromBrand);
+const UtilityCard = ({ data, onCardClick, detailed = true, cardStyles = {} }) => {
+    const wallet = useStore((state) => state.wallet);
+    const notifyUser = useStore((state) => state.notifyUser);
+    const getKeywordFromBrand = useStore((state) => state.getKeywordFromBrand);
 
     const ticketData = data.configuration ? data.configuration : data;
     const utilityValue = getValueFromSet(ticketData.utilityAmount);
@@ -35,13 +31,15 @@ const UtilityCard = ({
         }
 
         onCardClick(data);
-    }
+    };
 
     return (
-        <Card sx={{
-            ...cardDefaults,
-            ...cardStyles
-        }}>
+        <Card
+            sx={{
+                ...cardDefaults,
+                ...cardStyles,
+            }}
+        >
             <CardActionArea onClick={handleClick}>
                 <CardMedia
                     component="img"
@@ -49,7 +47,9 @@ const UtilityCard = ({
                     image={utilityValue.imagePath}
                     alt="utility"
                     sx={{
-                        pl: 4, pr: 4, pt: 2,
+                        pl: 4,
+                        pr: 4,
+                        pt: 2,
                         borderColor: 'onSurface.main',
                     }}
                 />
@@ -58,32 +58,31 @@ const UtilityCard = ({
                         <Typography gutterBottom variant="h5">
                             {ticketData.utilityTitle}
                         </Typography>
-                        <PhaseChip phase={data.phase}/>
+                        <PhaseChip phase={data.phase} />
                     </Stack>
                     {detailed ? (
-                            <>
-                                <Typography variant="subtitle1">
-                                    {Number(ticketData.minRentingDurationUnits)} to{' '}
-                                    {Number(ticketData.maxRentingDurationUnits)} {ticketData.rentingDurationUnit}s, to{' '}
-                                    {displayAmount(ticketData.rentalFeePerUnitAmount)} {getKeywordFromBrand(ticketData.rentalFeePerUnitAmount.brand)} per {ticketData.rentingDurationUnit}
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    {displayAmount(ticketData.collateralAmount)} {getKeywordFromBrand(ticketData.collateralAmount.brand)} of Collateral
-                                </Typography>
-                            </>
-                        ) :
-                        (
-                            <>
-                                <Typography variant="subtitle1">
-                                    {ticketData.utilityDescription}
-                                </Typography>
-                            </>
-                        )
-                    }
+                        <>
+                            <Typography variant="subtitle1">
+                                {Number(ticketData.minRentingDurationUnits)} to{' '}
+                                {Number(ticketData.maxRentingDurationUnits)} {ticketData.rentingDurationUnit}s, to{' '}
+                                {displayAmount(ticketData.rentalFeePerUnitAmount)}{' '}
+                                {getKeywordFromBrand(ticketData.rentalFeePerUnitAmount.brand)} per{' '}
+                                {ticketData.rentingDurationUnit}
+                            </Typography>
+                            <Typography variant="subtitle1">
+                                {displayAmount(ticketData.collateralAmount)}{' '}
+                                {getKeywordFromBrand(ticketData.collateralAmount.brand)} of Collateral
+                            </Typography>
+                        </>
+                    ) : (
+                        <>
+                            <Typography variant="subtitle1">{ticketData.utilityDescription}</Typography>
+                        </>
+                    )}
                 </CardContent>
             </CardActionArea>
         </Card>
-    )
+    );
 };
 
 export default UtilityCard;
--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -4,13 +4,14 @@ import { useState } from 'react';
 import { FilterBar } from '../../components/FilterBar/FilterBar.jsx';
 import { Bag } from '../../components/Bag/Bag.jsx';
 import useStore from '../../store/store.js';
-import Typography from "@mui/material/Typography";
-import { Box, Paper } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import UtilityCard from "../../components/UtilityCard.jsx";
-import BorrowRentalDialog from "../../components/BorrowRentalDialog.jsx";
-import NothingToShow from "../../components/NothingToShow.jsx";
-import { EmptyTexts } from "../../utils/constants.js";
+import Typography from '@mui/material/Typography';
+import { Box, Paper } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import UtilityCard from '../../components/UtilityCard.jsx';
+import BorrowRentalDialog from '../../components/BorrowRentalDialog.jsx';
+import NothingToShow from '../../components/NothingToShow.jsx';
+import { EmptyTexts } from '../../utils/constants.js';
+import { catalogData } from '../../utils/mockData.js';
 
 export const Explore = ({ bagOpen }) => {
     const catalog = useStore((state) => state.catalog) || [];
@@ -18,12 +19,13 @@ export const Explore = ({ bagOpen }) => {
     const [borrowOpen, setBorrowOpen] = useState(false);
     console.log('activeTicket', activeTicket);
 
-    const displayData = [...catalog].filter(({ phase }) => phase === 'available');
-    // const displayData = catalogData;
+    // const displayData = [...catalog].filter(({ phase }) => phase === 'available');
+    const displayData = catalogData;
 
-    const handleCardClick = rentalData => {
-      setActiveTicket(rentalData);
-      setBorrowOpen(true);
+    const handleCardClick = (rentalData) => {
+        console.log('clccccc ');
+        setActiveTicket(rentalData);
+        setBorrowOpen(true);
     };
 
     const closeActiveTicket = () => {
@@ -33,57 +35,64 @@ export const Explore = ({ bagOpen }) => {
 
     const displayBody = () => {
         if (displayData.length === 0) {
-            return (<NothingToShow message={EmptyTexts.CATALOG}/>)
+            return <NothingToShow message={EmptyTexts.CATALOG} />;
         }
 
         return (
-            <Grid container spacing={4} className='GRID'>
+            <Grid container spacing={4} className="GRID">
                 {displayData.map((data, index) => (
-                    <Grid key={index * 10} item xs={4}
-                          sx={{ display: 'flex', justifyContent: 'center' }}>
-                        <UtilityCard key={index} data={data} onCardClick={handleCardClick}/>
+                    <Grid key={index * 10} item xs={4} sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <UtilityCard key={index} data={data} onCardClick={handleCardClick} />
                     </Grid>
                 ))}
             </Grid>
-        )
-    }
+        );
+    };
 
     return (
-        <Box className="catalog" sx={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            alignItems: 'center',
-            pb: 2,
-        }}>
-            <Typography variant="h3" align='center' m={2} color={'surface.contrastText'}>Rent whatever you
-                want</Typography>
+        <Box
+            className="catalog"
+            sx={{
+                width: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignItems: 'center',
+                pb: 2,
+            }}
+        >
+            <Typography variant="h3" align="center" m={2} color={'surface.contrastText'}>
+                Rent whatever you want
+            </Typography>
 
-            <Paper sx={{
-                width: '70%',
-                minWidth: 0,
-                minHeight: 0,
-                height: '100vh',
-                overflow: 'auto',
-                p: 2,
-                bgcolor: 'surface.main',
-                borderRadius: (theme) => theme.spacing(2),
-                boxShadow: '0px 0px 80px 0px rgba(0,0,0,0.75)'
-            }} elevation={3} className='Paper'>
+            <Paper
+                sx={{
+                    width: '70%',
+                    minWidth: 0,
+                    minHeight: 0,
+                    height: '100vh',
+                    overflow: 'auto',
+                    p: 2,
+                    bgcolor: 'surface.main',
+                    borderRadius: (theme) => theme.spacing(2),
+                    boxShadow: '0px 0px 80px 0px rgba(0,0,0,0.75)',
+                }}
+                elevation={3}
+                className="Paper"
+            >
                 {bagOpen ? (
-                    <Bag/>
+                    <Bag />
                 ) : (
                     <>
-                        <Box sx={{ width: 1}}>
-                            <FilterBar/>
+                        <Box sx={{ width: 1 }}>
+                            <FilterBar />
                         </Box>
 
                         {displayBody()}
                     </>
                 )}
             </Paper>
-            <BorrowRentalDialog open={borrowOpen} rentalData={activeTicket} onClose={closeActiveTicket}/>
+            <BorrowRentalDialog open={borrowOpen} rentalData={activeTicket} onClose={closeActiveTicket} />
         </Box>
     );
 };
